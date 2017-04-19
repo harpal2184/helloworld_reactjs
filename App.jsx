6c1ebae8cc6bc9f8edd02f8b28d7ed34383be1a5
Harpalsinh Chauhan <harpal@Harpalsinhs-MacBook-Pro.local>
2017-04-19 15:51:47 -0400

    api call react

--- App.jsx
+++ App.jsx
@@ -1,10 +1,39 @@
 import React from 'react';
+import Request from 'superagent';
+import _  from 'lodash';
 
 class App extends React.Component {
+	constructor(){
+		super();
+		this.state = {
+			text: 'hello world'
+		};
+	}
+	componentWillMount(){
+		var url = "http://www.omdbapi.com/?s=star&y=&r=json$plot=short";
+		Request.get(url).then((response) => {
+			this.setState({
+				movies : response.body.Search
+				
+			});
+		});
+	}
+	clicked(){
+		this.setState({text: this.refs.textBox.value});
+
+	}
 	render() {
+		const img = ("./images/nature1.jpg");
+		var movies = _.map(this.state.movies, (movie) => {
+			return <li key={movie.imdbID}>{movie.Title}</li>;
+		});
 		return (
 			<div>
-				Hello World!!!
+				{this.state.text }
+				<ul>{movies}</ul>
+				<p>Hello World!!!</p>
+				<input type="text" ref="textBox" />
+				<button onClick={ (e) => {e.preventDefault(); this.clicked(); } }>Button</button>
 			</div>
 			);
 	}
